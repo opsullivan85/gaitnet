282b075bd1b7a2d9a1bfac966d1a4a2ead7b6906
owen-sullivan <opsullivan85@gmail.com>
2025-12-31 15:28:46 -0500

    .

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="gaitnet",
     version="0.1.0",
     description="Greedy Dynamic Quadruped Gait",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(),
+    package_dir={"": "."},
     install_requires=[
         "torch",
         "rsl-rl @ git+https://github.com/leggedrobotics/rsl_rl.git@v1.0.2"
